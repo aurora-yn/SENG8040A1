3cc7b4048e8673c0ddd12314d38ba021f784961c
Aurora <yeongyun.nk@gmail.com>
2020-10-03 21:07:12 -0400

    Created the triangle program

---
+++ SENG8040A1/SENG8040A1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SENG8040A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunProg();
+        }
+        public static void RunProg()
+        {
+            Console.WriteLine("Select a menu using number. \n" +
+                "1. Enter triangle dimensions \n" +
+                "2. Exit");
+
+            int selectedMenu = GetInput();
+            while (selectedMenu != 1 && selectedMenu != 2)
+            {
+                Console.WriteLine("Enter only exsiting menu numbers.");
+                selectedMenu = GetInput();
+            }
+
+            if (selectedMenu == 1)
+            {
+                Console.WriteLine("selected 1. Enter triangle dimensions");
+                GetTriangle();
+                Restart();
+            }
+            else if (selectedMenu == 2)
+            {
+                Console.WriteLine("selected 2. Exit");
+                Environment.Exit(0);
+            }
+        }
+        public static void Restart()
+        {
+            Console.WriteLine("Would you like to restart the program? If so, enter 'y'.");
+            string restartInput = Console.ReadLine().ToLower();
+            if (restartInput == "y")
+            {
+                RunProg();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        public static void GetTriangle()
+        {
+            Console.WriteLine("Enter first dimension.");
+            int firstD = GetInput();
+
+            Console.WriteLine("Enter second dimension.");
+            int secondD = GetInput();
+
+            Console.WriteLine("Enter thrid dimension.");
+            int thridD = GetInput();
+
+            TriangleSolver triangleSolver = new TriangleSolver();
+            triangleSolver.Analyze(firstD, secondD, thridD);
+        }
+        public static int GetInput()
+        {
+            int number;
+            string input = Console.ReadLine();
+            while (int.TryParse(input, out int x) == false)
+            {
+                Console.WriteLine("It is Invalid type. Enter only number.");
+                input = Console.ReadLine();
+            }
+            number = Int32.Parse(input);
+            return number;
+        }
+    }
+}
---
+++ SENG8040A1/SENG8040A1/TriangleSolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SENG8040A1
+{
+    public class TriangleSolver
+    {
+        public void Analyze(int firstD, int secondD, int thirdD)
+        {
+            Console.WriteLine($"\nTriangle dimensions entered are {firstD}, {secondD}, and {thirdD}.");
+            //int[] dimensions = { firstD, secondD, thirdD };
+
+            if (firstD == secondD && firstD == thirdD)
+            {
+                Console.WriteLine($"This triangle is an equilateral.\n");
+            }
+            else if (firstD == secondD || firstD == thirdD || secondD == thirdD)
+            {
+                Console.WriteLine($"This triangle is an isosceles.\n");
+            } 
+            else
+            {
+                Console.WriteLine($"This triangle is a scalene.\n");
+            }
+        }
+    }
+}
